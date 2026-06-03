04b7411c9d8acbb7a8a7bcf407b3bfba9911c72a
Sameuler <samxhdc@gmail.com>
2026-06-03 22:24:29 +0200

    feat: add percentage performance metrics of the selected stocks

--- src/dashboard.py
+++ src/dashboard.py
@@ -23,13 +23,26 @@ try:
 except Exception as e:
     st.error(f"Failed to connect: {e}")
     st.stop()
-query = "SELECT * FROM historic_prices ORDER BY date_time DESC LIMIT 100;"
+query = "SELECT * FROM historic_prices ORDER BY date_time DESC;"
 df = pd.read_sql(query, connection)
 
 df_sorted=df.sort_values(by="date_time")
-st.subheader("Latest Database Market Prices")
-st.dataframe(df)
 
+st.subheader("Portfolio Performance Metrics")
+
+unique_tickers =df_sorted['ticker_symbol'].unique()
+cols=st.columns(len(unique_tickers))
+for col, ticker in zip(cols, unique_tickers):
+    ticker_df=df[df['ticker_symbol']==ticker].sort_values(by="date_time")
+    if not ticker_df.empty:
+        initial_price=ticker_df['price'].iloc[0]
+        latest_price=ticker_df['price'].iloc[-1]
+        pct_change=(latest_price-initial_price)/initial_price
+        col.metric(
+            label=ticker,
+            value=f"${latest_price:.2f}",
+            delta=f"${pct_change:+.2f}"
+        )
 fig=px.line(
     df,
     x="date_time",
