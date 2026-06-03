a3c2c2f5a4399dbba7a18388c989fcad38b7ccac
Sameuler <samxhdc@gmail.com>
2026-06-03 18:32:23 +0200

    feat: implement live data ingestion from yfinance API to mysql

--- src/ingest_data.py
+++ src/ingest_data.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import yfinance as yf
+import pandas as pd
+
 
 db = mysql.connector.connect(
     host="localhost",
@@ -8,5 +10,16 @@ db = mysql.connector.connect(
     database="market_prices"
 )
 cursor = db.cursor()
+
+ticker_symbol = input("Enter ticker symbol: ")
+ticker=yf.Ticker(ticker_symbol)
+historical_data=ticker.history(period="1d", interval="1m")
+pd.set_option('display.max_columns', None)
+price=historical_data["Close"].iloc[-1]
+time=historical_data.iloc[-1].name
+sql_query = "INSERT INTO historic_prices (ticker_symbol, date_time, price) VALUES (%s, %s, %s)"
+
+cursor.execute(sql_query, (ticker_symbol, time, price))
+db.commit()
 cursor.close()
 db.close()
