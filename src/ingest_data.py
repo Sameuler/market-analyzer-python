758dee2be77daf7e9272e0a8a132e7fa7adf6de8
Sameuler <samxhdc@gmail.com>
2026-06-03 20:50:53 +0200

    feat: implement automated timed loop to ingest the data with try...except...blocks to exit and clean connection

--- src/ingest_data.py
+++ src/ingest_data.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import yfinance as yf
 import pandas as pd
+import time as t
 
 
 db = mysql.connector.connect(
@@ -10,16 +11,22 @@ db = mysql.connector.connect(
     database="market_prices"
 )
 cursor = db.cursor()
-
 ticker_symbol = input("Enter ticker symbol: ")
-ticker=yf.Ticker(ticker_symbol)
-historical_data=ticker.history(period="1d", interval="1m")
-pd.set_option('display.max_columns', None)
-price=historical_data["Close"].iloc[-1]
-time=historical_data.iloc[-1].name
-sql_query = "INSERT INTO historic_prices (ticker_symbol, date_time, price) VALUES (%s, %s, %s)"
+ticker = yf.Ticker(ticker_symbol)
+try:
+    while True:
 
-cursor.execute(sql_query, (ticker_symbol, time, price))
-db.commit()
-cursor.close()
-db.close()
+        historical_data=ticker.history(period="1d", interval="1m")
+        pd.set_option('display.max_columns', None)
+        price=historical_data["Close"].iloc[-1]
+        time=historical_data.iloc[-1].name
+        sql_query = "INSERT INTO historic_prices (ticker_symbol, date_time, price) VALUES (%s, %s, %s)"
+        cursor.execute(sql_query, (ticker_symbol, time, price))
+        db.commit()
+        t.sleep(60)
+except KeyboardInterrupt:
+    print("\nManually stopped by user")
+finally:
+    cursor.close()
+    db.close()
+    print("\nGates Closed Successfully")
